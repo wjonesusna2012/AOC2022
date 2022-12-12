bcf26207df8beb01a1d9d9b5db23935f1f1d6718
wjonesusna2012 <44551434+wjonesusna2012@users.noreply.github.com>
2022-12-12 00:17:04 -0600

    Day11 Solutions with troubles with copying objects

--- Day11/Day11.cpp
+++ Day11/Day11.cpp
@@ -3,9 +3,9 @@
 #include <functional>
 #include <vector>
 
-std::function<int(int)> createDivisibleBy(int divisor, int trueMonkey, int falseMonkey)
+std::function<long(long)> createDivisibleBy(long divisor, long trueMonkey, long falseMonkey)
 {
-  return [divisor, trueMonkey, falseMonkey](int a) -> int
+  return [divisor, trueMonkey, falseMonkey](long a) -> long
   {
     return a % divisor == 0 ? trueMonkey : falseMonkey;
   };
@@ -13,48 +13,100 @@ std::function<int(int)> createDivisibleBy(int divisor, int trueMonkey, int false
 class Monkey
 {
 public:
-  int itemsProcessed{0};
-  std::vector<int> heldItems;
-  std::function<int(int)> operationFunction;
-  std::function<int(int)> testFunction;
+  long itemsProcessed{0};
+  std::vector<long> heldItems;
+  std::function<long(long)> operationFunction;
+  std::function<long(long)> testFunction;
 
-  Monkey(std::vector<int> sI, std::function<int(int)> oF, std::function<int(int)> tF)
+  Monkey(std::vector<long> sI, std::function<long(long)> oF, std::function<long(long)> tF)
   {
     heldItems = sI;
     operationFunction = oF;
     testFunction = tF;
   }
-  void addItem(int item)
+  void addItem(long item)
   {
     heldItems.push_back(item);
   }
-  std::vector<std::pair<int, int>> processItems()
+  std::vector<std::pair<long, long>> processItems()
   {
-    std::vector<std::pair<int, int>> redistributedItems;
+    std::vector<std::pair<long, long>> redistributedItems;
     for (auto i : heldItems)
     {
-      int adjusted = operationFunction(i);
-      // Divide by 3 after monkey doesn't break it. Floor is applied by default for int division
+      long adjusted = operationFunction(i);
+      // Divide by 3 after monkey doesn't break it. Floor is applied by default for long division
       adjusted /= 3;
-      int toMonkey = testFunction(adjusted);
-      redistributedItems.emplace_back(toMonkey, adjusted);
+      long toMonkey = testFunction(adjusted);
+      redistributedItems.emplace_back(toMonkey, adjusted % (2 * 7 * 3 * 17 * 11 * 19 * 5 * 13));
     }
+    itemsProcessed += heldItems.size();
     return redistributedItems;
+  }
+  void clearItems()
+  {
+    heldItems.clear();
   }
 };
 
 int main(int argc, char *argv[])
 {
-  std::vector<Monkey> monkeyList;
+  std::vector<Monkey *> monkeyList;
   // MOnkey 0
   monkeyList.emplace_back(
-      std::vector<int>{80}, [](int a) -> int
+      std::vector<long>{80}, [](long a) -> long
       { return a * 5; },
       createDivisibleBy(2, 4, 3));
   // Monkey 1
   monkeyList.emplace_back(
-      std::vector<int>{75, 83, 74}, [](int a) -> int
+      std::vector<long>{75, 83, 74}, [](long a) -> long
       { return a + 7; },
       createDivisibleBy(7, 5, 6));
+  // Monkey 2
+  monkeyList.emplace_back(
+      std::vector<long>{86, 67, 61, 96, 52, 63, 73}, [](long a) -> long
+      { return a + 5; },
+      createDivisibleBy(3, 7, 0));
+  // Monkey 3
+  monkeyList.emplace_back(
+      std::vector<long>{85, 83, 55, 85, 57, 70, 85, 52}, [](long a) -> long
+      { return a + 8; },
+      createDivisibleBy(17, 1, 5));
+  // Monkey 4
+  monkeyList.emplace_back(
+      std::vector<long>{67, 75, 91, 72, 89}, [](long a) -> long
+      { return a + 4; },
+      createDivisibleBy(11, 3, 1));
+  // Monkey 5
+  monkeyList.emplace_back(
+      std::vector<long>{66, 64, 68, 92, 68, 77}, [](long a) -> long
+      { return a * 2; },
+      createDivisibleBy(19, 6, 2));
+  // Monkey 6
+  monkeyList.emplace_back(
+      std::vector<long>{97, 94, 79, 88}, [](long a) -> long
+      { return a * a; },
+      createDivisibleBy(5, 2, 7));
+  // Monkey 7
+  monkeyList.emplace_back(
+      std::vector<long>{77, 85}, [](long a) -> long
+      { return a + 6; },
+      createDivisibleBy(13, 4, 0));
+  const auto ROUNDS = 20;
+  for (long i = 0; i < ROUNDS; i++)
+  {
+    for (long m = 0; m < monkeyList.size(); m++)
+    {
+      auto itemsToAdd = monkeyList[m]->processItems();
+      monkeyList[m]->clearItems();
+      for (auto j : itemsToAdd)
+      {
+        monkeyList[j.first]->addItem(j.second);
+      }
+    }
+  }
+  for (auto x : monkeyList)
+  {
+    std::cout << x->itemsProcessed << std::endl;
+  }
   return EXIT_SUCCESS;
 }
